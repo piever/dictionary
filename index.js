d6cba66888c05f0e35ba062193b181bb8914cd3e
piever <pietro.vertechi@protonmail.com>
2021-08-29 12:33:38 +0200

    reorganize into components

--- index.js
+++ index.js
@@ -1,54 +1,117 @@
-const ui = {
-    view: function () {
-        const textBox = m(
+function many(tag, component) {
+    return {
+        view: function (vnode) {
+            const entries = vnode.attrs.entries || [];
+            return m(tag, entries.map(entry => m(component, entry)));
+        }
+    };
+}
+
+const textBox = {
+    view: function (vnode) {
+        return m(
             "input",
             {
                 type: "text",
-                onchange: ev => ui.getWord(ev.target.value),
-                class: "px-2 py-1 placeholder-gray-400 rounded border border-gray-400 outline-none focus:outline-none focus:ring w-full",
+                onchange: vnode.attrs.onchange,
+                class: "px-2 py-1 placeholder-gray-400 rounded border border-gray-400 w-full",
                 placeholder: "Enter a word here"
             }
         );
-        const error = ui.result.error ? [m("p.mt-2", ui.result.error)] : []
-        const phonetics = m("div.mt-2", (ui.result.phonetics || []).map(function (entry) {
-            const id = uuidv4();
-            const audio = entry.audio ? [
-                m("audio", {id: id, src: entry.audio}),
-                m(
-                    "a.cursor-pointer",
-                    {onclick: () => document.getElementById(id).play()},
-                    "🔊 "
-                ),
-                m("span.ml-2")
-            ] : [m("span.ml-2")];
-            return [m("span", entry.text + " "), audio];
-        }));
-        const meanings = ( ui.result.meanings || []).map(function (value) {
-            const partOfSpeech = m("p.font-bold.mt-2", value.partOfSpeech);
-            const definitions = value.definitions.map(function (def) {
-                var example = def.example ? [
-                    m("span.text-blue-600", " Example: "),
-                    m("span.text-blue-600.italic", def.example)
-                ] : [];
-                return m("li", m("span", def.definition), example);
-            });
-            return m("div", [partOfSpeech, m("ol.list-decimal.list-inside", definitions)]);
-        });
-        const origin = ui.result.origin ? [m("p.font-bold.mt-2", "origin"), ui.result.origin] : [];
-        return m("div.my-4.mx-8.max-w-4xl", [textBox, error, phonetics, meanings, origin]);
+    }
+};
+
+const error = {
+    view: function (vnode) {
+        const text = vnode.attrs.text;
+        return text ? [m("p.mt-2", text)] : [];
+    }
+};
+
+const player = {
+    view: function (vnode) {
+        const src = vnode.attrs.src;
+        const margin = m("span.ml-2");
+        if (src) {
+            const audio = m("audio", {src});
+            const play = m("a.cursor-pointer", {onclick: () => audio.dom.play()}, "🔊 ");
+            return [audio, play, margin];
+        } else {
+            return [margin];
+        }
+    }
+};
+
+const phonetic = {
+    view: function (vnode) {
+        return [m("span", vnode.attrs.text + " "), m(player, {src: vnode.attrs.audio})];
+    }
+};
+
+const phonetics = many("div.mt-2", phonetic);
+
+const definition = {
+    view: function (vnode) {
+        const example = vnode.attrs.example ? [
+            m("span.text-blue-600", " Example: "),
+            m("span.text-blue-600.italic", vnode.attrs.example)
+        ] : [];
+        return m("li", m("span", vnode.attrs.definition), example);
+    }
+};
+
+const definitions = many("ol.list-decimal.list-inside", definition);
+
+const partOfSpeech = {
+    view: function (vnode) {
+        const text = vnode.attrs.text;
+        return m("p.font-bold.mt-2", text);
+    }
+};
+
+const meaning = {
+    view: function (vnode) {
+        return m("div", [
+            m(partOfSpeech, {text: vnode.attrs.partOfSpeech}),
+            m(definitions, {entries: vnode.attrs.definitions})
+        ]);
+    }
+};
+
+const meanings = many("div", meaning);
+
+const origin = {
+    view: function (vnode) {
+        const text = vnode.attrs.text;
+        return text ? [m("p.font-bold.mt-2", "origin"), m("p", text)] : [];
+    }
+};
+
+const ui = {
+    view: function () {
+        const components = [
+            m(textBox, {onchange: ev => ui.loadResult(ev.target.value)}),
+            m(error, {text: ui.result.error}),
+            m(phonetics, {entries: ui.result.phonetics}),
+            m(meanings, {entries: ui.result.meanings}),
+            m(origin, {text: ui.result.origin})
+        ]
+        return m("div.my-4.mx-8.max-w-4xl", components);
     },
+
     result: {},
-    getWord: function (word) {
+
+    loadResult: function (word) {
         return m.request({
             method: "GET",
             url: "https://api.dictionaryapi.dev/api/v2/entries/en/" + word,
         }).then(function(result) {
             ui.result = result[0];
         }).catch(function(e) {
-            ui.result = {error: "word not found"};
+            ui.result = {error: "Word not found."};
         })
     }
-}
+};
 
 document.body.classList.add("bg-gray-100");
 
